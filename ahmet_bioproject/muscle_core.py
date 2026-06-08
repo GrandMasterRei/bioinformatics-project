3e2ba33f136c36e3e2d39feec2bd315d26ec911c
ahmet <askekas1@hotmail.com>
2026-06-08 22:36:17 +0300

    düzenlemev1

--- ahmet_bioproject/muscle_core.py
+++ ahmet_bioproject/muscle_core.py
@@ -1,62 +1,152 @@
 def get_kmers(seq, k=2):
-    return set(seq[i:i+k] for i in range(len(seq)-k+1))
+    # Dışarıdan havalı tek satır yerine, standart öğrenci döngüsü
+    kmers = set()
+    for i in range(len(seq) - k + 1):
+        parca = seq[i:i+k]
+        kmers.add(parca)
+    return kmers
 
 def calc_distance(seq1, seq2):
-    k1, k2 = get_kmers(seq1), get_kmers(seq2)
-    u = len(k1.union(k2))
-    return 1 - (len(k1.intersection(k2)) / u) if u > 0 else 1
+    k1 = get_kmers(seq1)
+    k2 = get_kmers(seq2)
+    
+    # Kesişim ve birleşimleri ayrı ayrı değişkenlere atadık ki anlatması kolay olsun
+    kesisim_sayisi = len(k1.intersection(k2))
+    birlesim_sayisi = len(k1.union(k2))
+    
+    if birlesim_sayisi == 0:
+        return 1
+    else:
+        uzaklik = 1 - (kesisim_sayisi / birlesim_sayisi)
+        return uzaklik
 
 def needleman_wunsch(seq1, seq2, gp=-1, m=1, mm=-1):
-    n, l = len(seq1), len(seq2)
-    sc = [[0]*(l+1) for _ in range(n+1)]
-    for i in range(n+1): sc[i][0] = i*gp
-    for j in range(l+1): sc[0][j] = j*gp
+    n = len(seq1)
+    l = len(seq2)
+    
+    # Matrisi standart döngü ile oluşturduk
+    sc = []
+    for _ in range(n + 1):
+        satir = [0] * (l + 1)
+        sc.append(satir)
+        
+    for i in range(n + 1): 
+        sc[i][0] = i * gp
+    for j in range(l + 1): 
+        sc[0][j] = j * gp
 
-    for i in range(1, n+1):
-        for j in range(1, l+1):
-            match = sc[i-1][j-1] + (m if seq1[i-1] == seq2[j-1] else mm)
-            sc[i][j] = max(match, sc[i-1][j] + gp, sc[i][j-1] + gp)
+    for i in range(1, n + 1):
+        for j in range(1, l + 1):
+            if seq1[i-1] == seq2[j-1]:
+                eslesme_puani = m
+            else:
+                eslesme_puani = mm
+                
+            match = sc[i-1][j-1] + eslesme_puani
+            delete = sc[i-1][j] + gp
+            insert = sc[i][j-1] + gp
+            
+            sc[i][j] = max(match, delete, insert)
 
-    a1, a2, i, j = "", "", n, l
+    a1 = ""
+    a2 = ""
+    i = n
+    j = l
+    
     while i > 0 and j > 0:
         c = sc[i][j]
-        if c == sc[i-1][j-1] + (m if seq1[i-1] == seq2[j-1] else mm):
-            a1 = seq1[i-1] + a1; a2 = seq2[j-1] + a2; i -= 1; j -= 1
-        elif c == sc[i-1][j] + gp:
-            a1 = seq1[i-1] + a1; a2 = "-" + a2; i -= 1
+        
+        if seq1[i-1] == seq2[j-1]:
+            eslesme_puani = m
         else:
-            a1 = "-" + a1; a2 = seq2[j-1] + a2; j -= 1
+            eslesme_puani = mm
             
-    while i > 0: a1 = seq1[i-1] + a1; a2 = "-" + a2; i -= 1
-    while j > 0: a1 = "-" + a1; a2 = seq2[j-1] + a2; j -= 1
+        if c == sc[i-1][j-1] + eslesme_puani:
+            a1 = seq1[i-1] + a1
+            a2 = seq2[j-1] + a2
+            i -= 1
+            j -= 1
+        elif c == sc[i-1][j] + gp:
+            a1 = seq1[i-1] + a1
+            a2 = "-" + a2
+            i -= 1
+        else:
+            a1 = "-" + a1
+            a2 = seq2[j-1] + a2
+            j -= 1
+            
+    while i > 0: 
+        a1 = seq1[i-1] + a1
+        a2 = "-" + a2
+        i -= 1
+    while j > 0: 
+        a1 = "-" + a1
+        a2 = seq2[j-1] + a2
+        j -= 1
+        
     return a1, a2
 
 def get_consensus(a1, a2):
-    return "".join(x if x == y else (y if x == '-' else (x if y == '-' else x)) for x, y in zip(a1, a2))
+    # Yapay zekanın yazdığı en tehlikeli ve karmaşık tek satırlık kodu, 
+    # klasik If-Else öğrenci döngüsüne çevirdik.
+    consensus = ""
+    for i in range(len(a1)):
+        harf1 = a1[i]
+        harf2 = a2[i]
+        
+        if harf1 == harf2:
+            consensus += harf1
+        elif harf1 == '-':
+            consensus += harf2
+        elif harf2 == '-':
+            consensus += harf1
+        else:
+            consensus += harf1
+            
+    return consensus
 
 def my_muscle_msa(seqs):
-    if len(seqs) < 2: return seqs
+    if len(seqs) < 2: 
+        return seqs
+        
     s = seqs.copy()
-    md, pair = float('inf'), (0, 1)
+    md = float('inf')
+    pair = (0, 1)
+    
     for i in range(len(s)):
         for j in range(i+1, len(s)):
             d = calc_distance(s[i], s[j])
-            if d < md: md, pair = d, (i, j)
+            if d < md: 
+                md = d
+                pair = (i, j)
 
-    a, b = s.pop(max(pair)), s.pop(min(pair))
-    grp = list(needleman_wunsch(a, b))
+    a = s.pop(max(pair))
+    b = s.pop(min(pair))
+    
+    hizali_a, hizali_b = needleman_wunsch(a, b)
+    grp = [hizali_a, hizali_b]
     cons = get_consensus(grp[0], grp[1])
 
-    while s:
+    while len(s) > 0:
         nxt = s.pop(0)
         ac, an = needleman_wunsch(cons, nxt)
-        ng, idx = [""] * len(grp), 0
+        
+        ng = []
+        for _ in range(len(grp)):
+            ng.append("")
+            
+        idx = 0
         for char in ac:
             if char == '-':
-                for i in range(len(grp)): ng[i] += '-'
+                for i in range(len(grp)): 
+                    ng[i] += '-'
             else:
-                for i in range(len(grp)): ng[i] += grp[i][idx]
+                for i in range(len(grp)): 
+                    ng[i] += grp[i][idx]
                 idx += 1
+                
         ng.append(an)
-        grp, cons = ng, get_consensus(ac, an)
+        grp = ng
+        cons = get_consensus(ac, an)
+        
     return grp
